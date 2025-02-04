a58158eb856a996e42c9add3dfc0e2d272eae3c5
Riya20-shah <105718882+Riya20-shah@users.noreply.github.com>
2025-02-04 08:43:19 +0000

    chnage in test.pt file

--- Test.py
+++ Test.py
@@ -1,1 +1,20 @@
-print("Hello World")
+class MyClass:
+    def __init__(self):
+        # Constructor method
+        self.value = 0
+
+    def increment(self, amount):
+        """
+        Increment the value by the given amount.
+        
+        :param amount: The amount to increment the value by.
+        """
+        self.value += amount
+
+    def get_value(self):
+        """
+        Get the current value.
+        
+        :return: The current value.
+        """
+        return self.value
